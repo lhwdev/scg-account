d274d86518d7de174dc856adb1d0fff7ada9dac2
lhwdev <lhwdev6@outlook.com>
2023-03-19 21:19:19 +0900

    ...

--- packages/utils/api/src/builder.ts
+++ packages/utils/api/src/builder.ts
@@ -3,28 +3,5 @@ import { Api, ApiCommon, ApiEntity, ApiLibrary, ApiManager, Path } from "./type"
 type Builder<Manager extends ApiManager> = (scope: BuildScope<Manager>) => void;
 
 export class BuildScope<Manager extends ApiManager> {
-  private apis = new Map<Path, Api<Manager>>();
-  private extras: ApiCommon<Manager> = {
-    metadata: {},
-  };
-
-  library(name: Path, block: Builder<Manager>) {
-    const scope = new BuildScope<Manager>();
-    block(scope);
-    this.apis.set(name, scope.build());
-  }
-
-  entity(name: Path, value: ApiEntity<Manager>) {
-    this.apis.set(name, value);
-  }
-
-  build(): ApiLibrary<Manager> {
-    return { ...Object.fromEntries(this.apis), metadata: this.extras };
-  }
-}
-
-export function buildApi<Manager extends ApiManager>(block: Builder<Manager>): ApiLibrary<Manager> {
-  const scope = new BuildScope<Manager>();
-  block(scope);
-  return scope.build();
+  
 }
--- packages/utils/api/src/type.ts
+++ packages/utils/api/src/type.ts
@@ -3,7 +3,11 @@ export type ApiManager = {
   metadata: Record<string | number | symbol, unknown | undefined> & {
     deprecated?: Deprecated;
   };
-  paramHandler: 
+  paramInput: unknown,
+  paramHandler(state: typeof this.paramInput): never;
+  resultHandler(state: typeof this.paramInput): never;
+
+  __thisIsOnlyForType: never;
 };
 
 export type ApiCommon<Manager extends ApiManager> = {
@@ -18,7 +22,7 @@ export type Deprecated = {
 export type Path =
   | "get" | "create" | "update" | "delete" // CRUD
   | "list" | "paginate" // CRUD - list
-  | "submit"
+  | "submit" | "upload"
   | "stream";
 
 export type ApiLibrary<Manager extends ApiManager> = {
@@ -26,24 +30,26 @@ export type ApiLibrary<Manager extends ApiManager> = {
 } & ApiCommon<Manager>;
 
 export type ApiEntity<Manager extends ApiManager> = {
-  [method in Manager["method"]]?: ApiCallable<Manager, never, unknown>;
+  [method in Manager["method"]]?: ApiCallable<Manager, never, ResultHandler<Manager, unknown> | undefined>;
 } & ApiCommon<Manager>;
 
 export type Method = string;
 
-export type Params = Record<string, unknown>
+export type Params<Manager extends ApiManager> = Record<string, ParamHandler<Manager, unknown>>
 
 export const ParamHandlerSymbol = Symbol("ParamHandler")
 
-type ParamHandler<Manager extends ApiManager, T> = { [ParamHandlerSymbol]:  }
+type ParamHandler<Manager extends ApiManager, T> = Manager["paramHandler"] & ((...args: never[]) => T)
 
-export interface ApiCallable<Manager extends ApiManager, in P extends Params, out R>
+type ResultHandler<Manager extends ApiManager, R> = Manager["resultHandler"] & ((...args: never[]) => R)
+
+export interface ApiCallable<Manager extends ApiManager, P extends Params<Manager>, R extends ResultHandler<Manager, unknown> | undefined>
   extends ApiCommon<Manager> {
-  
-  
+  params: P
+  result: R
 }
 
 export type Api<Manager extends ApiManager> =
   | ApiLibrary<Manager>
   | ApiEntity<Manager>
-  | ApiCallable<Manager, never, unknown>;
+  | ApiCallable<Manager, never, ResultHandler<Manager, unknown> | undefined>;
